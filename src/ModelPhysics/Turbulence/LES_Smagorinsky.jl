894e09026da402683f1335383ce3f97fcac5f48b
hudson_287 <egyhp1@nottingham.ac.uk>
2025-03-12 17:36:42 +0000

    Seemed like it should be this way instead

--- src/ModelPhysics/Turbulence/LES_Smagorinsky.jl
+++ src/ModelPhysics/Turbulence/LES_Smagorinsky.jl
@@ -7,9 +7,9 @@ export Smagorinsky
 Smagorinsky LES model containing all Smagorinksy field parameters.
 
 ### Fields
-- 'nut' -- Eddy viscosity ScalarField.
-- 'nutf' -- Eddy viscosity FaceScalarField.
-- 'coeffs' -- Model coefficients.
+- `nut` -- Eddy viscosity ScalarField.
+- `nutf` -- Eddy viscosity FaceScalarField.
+- `coeffs` -- Model coefficients.
 
 """
 struct Smagorinsky{S1,S2,C} <: AbstractLESModel
@@ -34,10 +34,10 @@ LES{Smagorinsky}(; C=0.15) = begin
 end
 
 # Functor as constructor (internally called by Physics API): Returns fields and user data
-(rans::LES{Smagorinsky,ARG})(mesh) where {ARG} = begin
+(les::LES{Smagorinsky,ARG})(mesh) where {ARG} = begin
     nut = ScalarField(mesh)
     nutf = FaceScalarField(mesh)
-    coeffs = rans.args
+    coeffs = les.args
     Smagorinsky(nut, nutf, coeffs)
 end
 
