8889263d944c296acacbc05405ea83c13952b7a7
hudson_287 <egyhp1@nottingham.ac.uk>
2025-03-18 16:17:01 +0000

    Created functions to clean up the `turbulence!` function

---
+++ src/ModelPhysics/Turbulence/DES_Functions.jl
@@ -0,0 +1,39 @@
+export menterF1!, cross_diffusion! , blend_nut!
+
+function menterF1!(des, model)
+    (; rho) = model.fluid
+    (; k, omega, nut, blnd_func, CDkw, y) = model.turbulence
+    (; βstar, σω2) = model.turbulence.coeffs
+    (; ω_eqn) = des
+
+    dkdomegadx = get_source(ω_eqn, 2)
+
+    @. CDkw.values = max((2 * rho.values * σω2 * (1 / omega.values) * dkdomegadx.values), 10e-20)
+    @. blnd_func.values = tanh(min(max(sqrt(k.values) / (βstar * y.values * omega.values),
+            (500 * nut.values) / (y.values^2 * omega.values)),
+        (4 * rho.values * σω2 * k.values) / (CDkw.values * y.values^2))^4)
+
+end
+
+function cross_diffusion!(des, model, config)
+
+    (; ω_eqn, ∇k, ∇ω) = des
+    (; σd) = model.turbulence.coeffs
+    (; k, omega, kf, omegaf, nutf) = model.turbulence
+
+    dkdomegadx = get_source(ω_eqn, 2)
+
+    interpolate!(kf, k, config)
+    correct_boundaries!(nutf, k, k.BCs, time, config)
+    interpolate!(omegaf, omega, config)
+    correct_boundaries!(nutf, omega, omega.BCs, time, config)
+    grad!(∇ω, omegaf, omega, omega.BCs, time, config)
+    grad!(∇k, kf, k, k.BCs, time, config)
+    inner_product!(dkdomegadx, ∇k, ∇ω, config)
+    @. dkdomegadx.values = max((σd / omega.values) * dkdomegadx.values, 0.0)
+end
+
+function blend_nut!(nut,blend,nutRANS,nutLES)
+    @. nut.values = (blend.values * nutRANS.values) + ((1 - blend.values) * nutLES.values)
+
+end
