50b3abdfc65b22b98567eaa33dd3360ddd9b8169
hudson_287 <egyhp1@nottingham.ac.uk>
2025-03-19 15:40:06 +0000

    Reflecting HM's suggestions on calling `blend!` with `blendType` as a parameter, update docs and how cross diffusion is done

--- src/ModelPhysics/Turbulence/DES_Functions.jl
+++ src/ModelPhysics/Turbulence/DES_Functions.jl
@@ -1,5 +1,4 @@
-function cross_diffusion!(des, model, config)
-
+function get_cross_diff(rans,des,model,config)
     (; ω_eqn, ∇k, ∇ω) = des
     (; σd) = model.turbulence.coeffs
     (; k, omega, kf, omegaf, nutf) = model.turbulence
@@ -14,35 +13,44 @@ function cross_diffusion!(des, model, config)
     grad!(∇k, kf, k, k.BCs, time, config)
     inner_product!(dkdomegadx, ∇k, ∇ω, config)
     @. dkdomegadx.values = max((σd / omega.values) * dkdomegadx.values, 0.0)
+
+    return dkdomegadx
 end
 
-function set_eddy_viscosity(model::Physics{T,F,M,Tu,E,D,BI}
-    ) where {T,F,M,Tu::Hybrid,E,D,BI}
+function get_cross_diff(rans::KOmegaLKE,des,model,config)
+    (; ω_eqn) = rans
+    dkdomegadx = get_source(ω_eqn,2)
+
+    return dkdomegadx
+end
+
+function set_eddy_viscosity(model::Physics) 
     (; nut, rans, les) = model.turbulence
     @. rans.nut.values = nut.values
     @. les.nut.values = nut.values
 end
 
 """
-    MenterF1(des::HybridModel, model::Physics)
+    blend!(blendType, des::HybridModel, model::Physics, config)
 
-Set the values of the `blendWeight` field according to the Menter F1 equation
+Set the values of the `blendWeight` field according to the method specified by `blendType`
 
 ### Input
+- `blendType` -- Blending method <: AbstractBlendingMethod
 - `des` -- turbulence model.
 - `model` -- Physics model defined by user.
+- `config` -- Configuration structure defined by user with solvers, schemes, runtime and 
+              hardware structures set.
 """
-function MenterF1(des::HybridModel, model::Physics, config)
+function blend!() end #Dummy for documentation
+
+function blend!(blendType::MenterF1, des::HybridModel, model::Physics, config)
     (; rho) = model.fluid
     (; k, omega, nut, blendWeight, CDkw, y, rans) = model.turbulence
     (; βstar, σω2) = model.turbulence.coeffs
     (; ω_eqn) = des
 
-    if !(typeof(rans) == KOmegaLKE)
-        cross_diffusion!(des, model, config)
-    end
-
-    dkdomegadx = get_source(ω_eqn, 2)
+    dkdomegadx = get_cross_diff(rans,des,model,config)
 
     @. CDkw.values = max((2 * rho.values * σω2 * (1 / omega.values) * dkdomegadx.values), 10e-20)
     @. blendWeight.values = tanh(min(max(sqrt(k.values) / (βstar * y.values * omega.values),
