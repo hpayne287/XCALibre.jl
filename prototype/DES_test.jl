b99314318a69bfbd52ff942af97cb43b614ca288
hudson_287 <egyhp1@nottingham.ac.uk>
2025-04-15 14:05:41 +0100

    Testing changes

--- prototype/DES_test.jl
+++ prototype/DES_test.jl
@@ -3,12 +3,12 @@ using XCALibre
 using Alert
 
 
-grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
-grid1 = "flatplate_2D_lowRe.unv"
+# grids_dir = pkgdir(XCALibre, "examples/0_GRIDS")
+# grid1 = "flatplate_2D_lowRe.unv"
 # grid1 = "bfs_unv_tet_10mm.unv"
-mesh_file = joinpath(grids_dir, grid1)
+# mesh_file = joinpath(grids_dir, grid1)
 
-# mesh_file = "C:/Users/Hudson/OneDrive - The University of Nottingham/Year 3/Individual Project/Code/Meshes/2025-03-25-Cylinder3D.unv"
+mesh_file = "C:/Users/egyhp1/OneDrive - The University of Nottingham/Year 3/Individual Project/Code/Meshes/2025-04-14-Case20Mesh2D.unv"
 
 mesh = UNV2D_mesh(mesh_file, scale=0.001)
 
@@ -18,17 +18,17 @@ hardware = set_hardware(backend=backend, workgroup=32)
 
 mesh_dev = mesh
 
-velocity = [0.5, 0.0, 0.0]
+velocity = [5.4, 0.0, 0.0]
 noSlip = [0.0,0.0,0.0]
 nu = 1.5e-5
 # Re = 23000
-k_inlet = 0.153
-ω_inlet = 1000
+k_inlet = 0.039 #3/2*(Tu*u_mag)^2
+ω_inlet = 1000 #k_inlet/(nuR*nu)
 
 model = Physics(
     time=Transient(),
     fluid=Fluid{Incompressible}(nu=nu),
-    turbulence=DES{Hybrid}(walls=(:wall,),blendType=MenterF2()), #walls=(:wall,) blendType=MenterF1()
+    turbulence=DES{Hybrid}(walls=(:plate,:upper),blendType=MenterF1()), #walls=(:wall,) blendType=MenterF1()
     energy=Energy{Isothermal}(),
     domain=mesh_dev
 )
@@ -38,45 +38,45 @@ model = Physics(
 @assign! model momentum U (
     Dirichlet(:inlet, velocity),
     Neumann(:outlet, 0.0),
-    Wall(:wall, [0.0, 0.0, 0.0]),
+    Wall(:plate, [0.0, 0.0, 0.0]),
     Neumann(:top,0.0),
-    # Neumann(:sides,0.0),
-    # Neumann(:bottom,0.0)
+    # Dirichlet(:upper,0.0),
+    Neumann(:preplate,0.0)
 )
 
 @assign! model momentum p (
     Neumann(:inlet, 0.0),
     Dirichlet(:outlet, 0.0),
-    Wall(:wall, 0.0),
+    Wall(:plate, 0.0),
     Neumann(:top,0.0),
-    # Neumann(:sides,0.0),
-    # Neumann(:bottom,0.0)
+    # Dirichlet(:upper,0.0),
+    Neumann(:preplate,0.0)
 )
 @assign! model turbulence k (
     Dirichlet(:inlet, k_inlet),
     Neumann(:outlet, 0.0),
-    Dirichlet(:wall,0.0),
+    Dirichlet(:plate,0.0),
     Neumann(:top,0.0),
-    # Neumann(:sides,0.0),
-    # Neumann(:bottom,0.0)
+    # Dirichlet(:upper,0.0),
+    Neumann(:preplate,0.0)
 )
 
 @assign! model turbulence omega (
     Dirichlet(:inlet, ω_inlet),
     Neumann(:outlet, 0.0),
-    OmegaWallFunction(:wall),
+    OmegaWallFunction(:plate),
     Neumann(:top,0.0),
-    # Neumann(:sides,0.0),
-    # Neumann(:bottom,0.0)
+    # Dirichlet(:upper,0.0),
+    Neumann(:preplate,0.0)
 )
 
 @assign! model turbulence nut (
     Dirichlet(:inlet, k_inlet / ω_inlet),
     Neumann(:outlet, 0.0),
-    Dirichlet(:wall,0.0),
+    Dirichlet(:plate,0.0),
     Neumann(:top,0.0),
-    # Neumann(:sides,0.0),
-    # Neumann(:bottom,0.0)
+    # Dirichlet(:upper,0.0),
+    Neumann(:preplate,0.0)
 )
 
 #endregion
@@ -135,7 +135,7 @@ solvers = (
     )
 )
 
-runtime = set_runtime(iterations=100, time_step=0.001, write_interval=10) #Adjust timestep to get a decent courant value
+runtime = set_runtime(iterations=4000, time_step=0.000025, write_interval=10) #Adjust timestep to get a decent courant value
 
 config = Configuration(
     solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
@@ -150,4 +150,4 @@ initialise!(model.turbulence.nut, k_inlet/ω_inlet)
 
 residuals = run!(model, config);
 
-alert("Simulation Done!!");
+# alert("Simulation Done!!");
