479561d4d73c80f9e7a1fb4e112ab86be7254b76
hudson_287 <egyhp1@nottingham.ac.uk>
2025-04-22 13:00:34 +0100

    Update _delta! kernel to differentiate between 2D and 3D meshes using multiple dispatch

--- src/ModelPhysics/Turbulence/LES_functions.jl
+++ src/ModelPhysics/Turbulence/LES_functions.jl
@@ -7,11 +7,24 @@ function delta!(Δ, mesh, config)
 
     # set up and launch kernel
     kernel! = _delta!(backend, workgroup)
-    kernel!(Δ, cells, ndrange = length(cells))
+    kernel!(Δ, cells, mesh, ndrange = length(cells))
     # KernelAbstractions.synchronize(backend)
 end
 
-@kernel function _delta!(Δ, cells) # Add types in call to get float type at compile time
+@kernel function _delta!(Δ, cells, mesh::Mesh3) # Add types in call to get float type at compile time
+    i = @index(Global)
+
+    @uniform begin
+        p = 1/3
+        values = Δ.values
+    end
+
+    @inbounds begin
+        values[i] = (cells[i].volume)^p
+    end
+end
+
+@kernel function _delta!(Δ, cells, mesh::Mesh2) #2D version
     i = @index(Global)
 
     @uniform begin
