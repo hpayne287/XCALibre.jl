e81dbb01160c92ada897eacece7dd3196886a159
hudson_287 <egyhp1@nottingham.ac.uk>
2025-05-02 14:15:46 +0100

    Working test case

--- prototype/DES_3D_BFS.jl
+++ prototype/DES_3D_BFS.jl
@@ -4,9 +4,8 @@ using Alert
 using StaticArrays
 using Accessors
 
-mesh_file = "C:/Users/Hudson/OneDrive - The University of Nottingham/Year 3/Individual Project/Code/Meshes/2025-04-28-Case31Mesh3D.unv"
-
-mesh = UNV3D_mesh(mesh_file, scale=0.001)
+mesh_file = "C:/Users/Hudson/OneDrive - The University of Nottingham/Year 3/Individual Project/Code/Meshes/2025-05-02-Case31Mesh2D.unv"
+mesh = UNV2D_mesh(mesh_file, scale=0.001)
 
 # Select backend and setup hardware
 backend = CPU()
@@ -14,56 +13,39 @@ hardware = set_hardware(backend=backend, workgroup=32)
 
 mesh_dev = mesh
 
-U_mag = 5.4
-L = 1.7
-velocity = [U_mag, 0.0, 0.0]
-noSlip = [0.0,0.0,0.0]
-nu = 1.5e-5
-Tu = 0.03
+reh = 5100
+nu = 1e-5
+h = 0.1
+Umag = reh*nu/h
+velocity = [Umag, 0.0, 0.0]
+noSlip = [0.0, 0.0, 0.0]
 
-# Re = 23000
-k_inlet = 3/2*(Tu*U_mag)^2
-# ω_inlet = (k_inlet^0.5)/(0.09^0.25)*L
-ω_inlet = 1000
+νR = 5
+Tu = 0.05
+k_inlet = 3/2*(Tu*Umag)^2
+ω_inlet = k_inlet/(νR*nu)
 nut∞ = 1e-15 
 
 model = Physics(
     time=Transient(),
     fluid=Fluid{Incompressible}(nu=nu),
-    turbulence=DES{Hybrid}(walls=(:wall,),blendType=MenterF1()),
+    turbulence=DES{Hybrid}(walls=(:wall1,:wall2,:side),blendType=MenterF1()),
     energy=Energy{Isothermal}(),
     domain=mesh_dev
 )
 
-function blasius_inlet(coords, time, index)
-    U∞ = 5.4        
-    ν = 1.5e-5       
-    x_virtual = 1      
-    y = coords[2]  - 0.0094
-    
-    y= max(y,0.0)
 
-    # Blasius similarity variable
-    η = y * sqrt(U∞ / (2 * ν * x_virtual))
-
-    # Blasius function approximation (f' = u/U∞)
-    f_prime = tanh(η / 2.6)  
-
-    # Velocity components
-    ux = U∞ * f_prime      
-    uy = 0.0               
-    uz = 0.0               
-
-    return SVector{3}(ux, uy, uz)
-end
 
 nutBCs = (
     Dirichlet(:inlet, k_inlet/ω_inlet),
     Neumann(:outlet, 0.0),
-    Dirichlet(:wall, 0.0),
-    Neumann(:top,0.0),
-    Neumann(:left, 0.0),
-    Neumann(:right, 0.0)
+    # Dirichlet(:wall,0.0),
+    Dirichlet(:wall1, 0.0),
+    Dirichlet(:wall2, 0.0),
+    Dirichlet(:side, 0.0),
+    Dirichlet(:top,0.0),
+    # Neumann(:left, 0.0),
+    # Neumann(:right, 0.0)
 )
 
 ransNut = assign(model.turbulence.rans.nut,nutBCs...)
@@ -75,48 +57,64 @@ lesNut = assign(model.turbulence.les.nut, nutBCs...)
 @assign! model turbulence nut (
     Dirichlet(:inlet, k_inlet/ω_inlet),
     Neumann(:outlet, 0.0),
-    Dirichlet(:wall, 0.0),
-    Neumann(:top,0.0),
-    Neumann(:left, 0.0),
-    Neumann(:right, 0.0)
+    # Dirichlet(:wall,0.0),
+    Dirichlet(:wall1, 0.0),
+    Dirichlet(:wall2, 0.0),
+    Dirichlet(:side, 0.0),
+    Dirichlet(:top,0.0),
+    # Neumann(:left, 0.0),
+    # Neumann(:right, 0.0)
 )
 
 #region Define BC
 @assign! model momentum U (
-    DirichletFunction(:inlet, blasius_inlet),
+    # DirichletFunction(:inlet, blasius_inlet),
+    Dirichlet(:inlet, velocity),
     Neumann(:outlet, 0.0),
-    Dirichlet(:wall, [0.0, 0.0, 0.0]),
-    Neumann(:top,0.0),
-    Neumann(:left, 0.0),
-    Neumann(:right, 0.0)
+    # Dirichlet(:wall, [0.0,0.0,0.0]),
+    Dirichlet(:wall1, [0.0, 0.0, 0.0]),
+    Dirichlet(:wall2, [0.0, 0.0, 0.0]),
+    Dirichlet(:side, [0.0, 0.0, 0.0]),
+    Dirichlet(:top, [0.0, 0.0, 0.0]),
+    # Neumann(:top,0.0),
+    # Neumann(:left, 0.0),
+    # Neumann(:right, 0.0)
 )
 
 @assign! model momentum p (
     Neumann(:inlet, 0.0),
-    Neumann(:outlet, 0.0),
-    Neumann(:wall, 0.0),
+    Dirichlet(:outlet, 0.0),
+    Neumann(:wall1, 0.0),
+    Neumann(:wall2, 0.0),
+    Neumann(:side, 0.0),
     Neumann(:top,0.0),
-    Neumann(:left, 0.0),
-    Neumann(:right, 0.0)
+    # Neumann(:left, 0.0),
+    # Neumann(:right, 0.0)
 )
 
 kcopy = assign(model.turbulence.rans.k, 
 Dirichlet(:inlet, k_inlet),
 Neumann(:outlet, 0.0),
-Dirichlet(:wall, 0.0),
-Neumann(:top,0.0),
-Neumann(:left, 0.0),
-Neumann(:right, 0.0)
+# Dirichlet(:wall, 0.0),
+Dirichlet(:wall1, 0.0),
+Dirichlet(:wall2, 0.0),
+Dirichlet(:side, 0.0),
+Dirichlet(:top,0.0),
+# Neumann(:left, 0.0),
+# Neumann(:right, 0.0)
 )
 @reset model.turbulence.rans.k = kcopy
 
 ωcopy = assign(model.turbulence.rans.omega, 
 Dirichlet(:inlet, ω_inlet),
 Neumann(:outlet, 0.0),
-Dirichlet(:wall, 1e6),
-Neumann(:top,0.0),
-Neumann(:left, 0.0),
-Neumann(:right, 0.0)
+# Dirichlet(:wall, 1e6),
+Dirichlet(:wall1, 1e6),
+Dirichlet(:wall2, 1e6),
+Dirichlet(:side, 1e6),
+Dirichlet(:top, 1e6),
+# Neumann(:left, 0.0),
+# Neumann(:right, 0.0)
 )
 @reset model.turbulence.rans.omega = ωcopy
 
@@ -156,7 +154,7 @@ solvers = (
         preconditioner = Jacobi(),
         convergence = 1e-8,
         relax       = 0.9,
-        itmax = 2000
+        itmax = 5000
     ),
     k = set_solver(
         model.turbulence.rans.k;
@@ -176,12 +174,14 @@ solvers = (
     )
 )
 
-runtime = set_runtime(iterations=10000, time_step=0.000001, write_interval=10) 
+runtime = set_runtime(iterations=10000, time_step=0.0005, write_interval=10) 
 
 config = Configuration(
     solvers=solvers, schemes=schemes, runtime=runtime, hardware=hardware)
+GC.gc()
 
-initialise!(model.momentum.U, [0.0,0.0,0.0]) 
+zeroV = [0.0,0.0,0.0]
+initialise!(model.momentum.U, velocity) 
 initialise!(model.momentum.p, 0.0)
 initialise!(model.turbulence.rans.k, k_inlet)
 initialise!(model.turbulence.rans.omega, ω_inlet)
